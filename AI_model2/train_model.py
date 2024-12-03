a01de8561caa47f6634d64ccb4af7c684d2e209c
Raisa <72346073+raisa05@users.noreply.github.com>
2024-12-03 01:56:19 +0000

    extraction of ingredients from json
    1. added more test data into json
    2. added a function to extract ingredient part

--- AI_model2/train_model.py
+++ AI_model2/train_model.py
@@ -1,5 +1,26 @@
 from datasets import load_dataset, Dataset
+import json
+
 
 #load data
 data_path = "recipes.json"
-dataset = load_dataset("json", data_files=data_path)
+with open(data_path, 'r') as f:
+    raw_data = json.load(f)
+
+
+#extraction of ingredients
+def extract_ingredient_features(ingredient, recipe_name):
+    def normalize_quantity(qty):
+        try:
+            numeric_value = float(re.sub(r'[^\d.]', '', str(qty))) #handle the quantity to take the numeric value
+            return numeric_value
+        except ValueError:
+            return 0.0
+
+    features = {
+        "base_text": f"{ingredient['quantity']} of {ingredient['ingredient_name']} in {recipe_name}",
+        "normalized_quantity": normalize_quantity(ingredient['quantity']),
+        "ingredient_name_length": len(ingredient['ingredient_name']),
+        "label": ingredient['importance']
+    }
+    return features
